8caeeed02d37c8f140afe150243cbe0afac3c328
Rafael Oliveira <ludug3r0@gmail.com>
2014-12-28 22:50:31 +0000

    Fixing lein deploy command
    - lein jar was deleting generated code from target
    - moving generated code from target to tmp

--- project.clj
+++ project.clj
@@ -13,25 +13,25 @@
                              :plugins      [[com.keminglabs/cljx "0.4.0" :exclusions [org.clojure/clojure]]
                                             [lein-cljsbuild "1.0.3"]
                                             [com.cemerick/clojurescript.test "0.3.3"]]}}
-            :source-paths ["target/generated/src/clj" "target/generated/src/cljs"]
-            :test-paths ["target/generated/test/clj"]
+            :source-paths ["tmp/generated/src/clj" "tmp/generated/src/cljs"]
+            :test-paths ["tmp/generated/test/clj"]
             :cljx {:builds [{:source-paths ["src/cljx"]
-                             :output-path  "target/generated/src/clj"
+                             :output-path  "tmp/generated/src/clj"
                              :rules        :clj}
                             {:source-paths ["src/cljx"]
-                             :output-path  "target/generated/src/cljs"
+                             :output-path  "tmp/generated/src/cljs"
                              :rules        :cljs}
                             {:source-paths ["test/cljx"]
-                             :output-path  "target/generated/test/clj"
+                             :output-path  "tmp/generated/test/clj"
                              :rules        :clj}
                             {:source-paths ["test/cljx"]
-                             :output-path  "target/generated/test/cljs"
+                             :output-path  "tmp/generated/test/cljs"
                              :rules        :cljs}]}
-            :cljsbuild {:builds        [{:source-paths ["target/generated/src/cljs" "target/generated/test/cljs"]
-                                         :compiler     {:output-to     "target/generated/js/test/testable.js"
+            :cljsbuild {:builds        [{:source-paths ["tmp/generated/src/cljs" "tmp/generated/test/cljs"]
+                                         :compiler     {:output-to     "tmp/generated/js/test/testable.js"
                                                         :optimizations :whitespace
                                                         :pretty-print  true}}]
                         :test-commands {"unit-tests" ["phantomjs" :runner
-                                                      "target/generated/js/test/testable.js"]}}
+                                                      "tmp/generated/js/test/testable.js"]}}
             :aliases {"deploy" ["do" "clean," "cljx" "once," "deploy" "clojars"]
                       "test"   ["do" "clean," "cljx" "once," "test," "with-profile" "dev" "cljsbuild" "test"]})
